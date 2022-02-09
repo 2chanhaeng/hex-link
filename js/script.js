89917c27c88315bbacd65893b5b7b9153348fb28
chomu37 <2chanhaeng@gmail.com>
2022-02-09 14:48:58 +0900

    Move up variants

--- js/script.js
+++ js/script.js
@@ -16,10 +16,31 @@ function getNew(){
 
 const lenHexes = 19;
 const idList = [...Array(lenHexes).keys()].map(i => String.fromCharCode(i + 97));
+
+const connectingCable = {
+    "a": {"b":1, "r":2, "q":4, "l":8, "d":16, "e":32},
+    "b": {"c":1, "s":2, "r":4, "a":8, "e":16, "f":32},
+    "c": {"m":1, "h":2, "s":4, "b":8, "f":16, "g":32},
+    "d": {"e":1, "a":2, "l":4, "p":8, "h":16, "i":32},
+    "e": {"f":1, "b":2, "a":4, "d":8, "i":16, "j":32},
+    "f": {"g":1, "c":2, "b":4, "e":8, "j":16, "k":32},
+    "g": {"q":1, "m":2, "c":4, "f":8, "k":16, "l":32},
+    "h": {"i":1, "d":2, "p":4, "s":8, "c":16, "m":32},
+    "i": {"j":1, "e":2, "d":4, "h":8, "m":16, "n":32},
+    "j": {"k":1, "f":2, "e":4, "i":8, "n":16, "o":32},
+    "k": {"l":1, "g":2, "f":4, "j":8, "o":16, "p":32},
+    "l": {"a":1, "q":2, "g":4, "k":8, "p":16, "d":32},
+    "m": {"n":1, "i":2, "h":4, "c":8, "g":16, "q":32},
+    "n": {"o":1, "j":2, "i":4, "m":8, "q":16, "r":32},
+    "o": {"p":1, "k":2, "j":4, "n":8, "r":16, "s":32},
+    "p": {"d":1, "l":2, "k":4, "o":8, "s":16, "h":32},
+    "q": {"r":1, "n":2, "m":4, "g":8, "l":16, "a":32},
+    "r": {"s":1, "o":2, "n":4, "q":8, "a":16, "b":32},
+    "s": {"h":1, "p":2, "o":4, "r":8, "b":16, "c":32},
+}
+
 let hexes = idList.reduce((acc,curr) => (acc[curr] = getNew(), acc), {});
-
-
-
+let checkedHexes = [];
 
 // 입력된 리스트에 포함된 수를 인덱스로 가지는 hexes의 요소에 getNew()를 적용하여 새로운 값을 대입
 function changeHex(list, obj){
@@ -72,27 +93,6 @@ function paraByCase(para, callback, one, two, four, eight, sixteen, thirtytwo, n
     caseby(para, function(){callback(one);}, function(){callback(two);}, function(){callback(four);}, function(){callback(eight);}, function(){callback(sixteen);}, function(){callback(thirtytwo);}, notofthem);
 }
 
-const connectingCable = {
-    "a": {"b":1, "r":2, "q":4, "l":8, "d":16, "e":32},
-    "b": {"c":1, "s":2, "r":4, "a":8, "e":16, "f":32},
-    "c": {"m":1, "h":2, "s":4, "b":8, "f":16, "g":32},
-    "d": {"e":1, "a":2, "l":4, "p":8, "h":16, "i":32},
-    "e": {"f":1, "b":2, "a":4, "d":8, "i":16, "j":32},
-    "f": {"g":1, "c":2, "b":4, "e":8, "j":16, "k":32},
-    "g": {"q":1, "m":2, "c":4, "f":8, "k":16, "l":32},
-    "h": {"i":1, "d":2, "p":4, "s":8, "c":16, "m":32},
-    "i": {"j":1, "e":2, "d":4, "h":8, "m":16, "n":32},
-    "j": {"k":1, "f":2, "e":4, "i":8, "n":16, "o":32},
-    "k": {"l":1, "g":2, "f":4, "j":8, "o":16, "p":32},
-    "l": {"a":1, "q":2, "g":4, "k":8, "p":16, "d":32},
-    "m": {"n":1, "i":2, "h":4, "c":8, "g":16, "q":32},
-    "n": {"o":1, "j":2, "i":4, "m":8, "q":16, "r":32},
-    "o": {"p":1, "k":2, "j":4, "n":8, "r":16, "s":32},
-    "p": {"d":1, "l":2, "k":4, "o":8, "s":16, "h":32},
-    "q": {"r":1, "n":2, "m":4, "g":8, "l":16, "a":32},
-    "r": {"s":1, "o":2, "n":4, "q":8, "a":16, "b":32},
-    "s": {"h":1, "p":2, "o":4, "r":8, "b":16, "c":32},
-}
 function isConnectable(id1, id2){
     return ((id1 in connectingCable && id2 in connectingCable)?connectingCable[id1][id2]:false) ? true : false;
 }
@@ -190,8 +190,6 @@ function isRinged(list){
     }
     return false;
 }
-
-let checkedHexes = [];
 
 // 모든 체크박스의 체크 상태를 취소합니다.
 function cancelAllCheckedCheckbox(){
