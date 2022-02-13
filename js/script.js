25537feb88af6cf7863c1aa1a6116064bf649bd3
chomu37 <2chanhaeng@gmail.com>
2022-02-13 17:51:06 +0900

    Coordinate pseudo cells

--- js/script.js
+++ js/script.js
@@ -40,8 +40,31 @@ idList.forEach(id => {
     view.appendChild(cell);
 });
 
-const boundaryList = ["a", "b", "c", "d", "g", "h", "l", "m", "p", "q", "r", "s"];
-boundaryList.forEach(id => {
+const vertexList = ["a", "c", "h", "l", "q", "s"];
+vertexList.forEach(id => {
+    for(let i = 0; i < 2; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('pseudo');
+        cell.classList.add('cell');
+        cell.id = `pseudo-${id}${i}`;
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `pseudo-${id}${i}-checkbox`;
+        cell.appendChild(checkbox);
+
+        const center = document.createElement('label');
+        center.classList.add('center');
+        center.setAttribute('for', `pseudo-${id}${i}-checkbox`);
+        center.innerHTML = '<div class="z pipe"></div>\n<div class="y pipe"></div>\n<div class="x pipe"></div>\n<div class="w pipe"></div>\n<div class="v pipe"></div>\n<div class="u pipe"></div>\n<div class="hexagon"></div>';
+        cell.appendChild(center);
+
+        view.appendChild(cell);
+    }
+});
+
+const sideList = ["b", "d", "g", "m", "p", "r"];
+sideList.forEach(id => {
     const cell = document.createElement('div');
     cell.classList.add('pseudo');
     cell.classList.add('cell');
