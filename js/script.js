4ab1534a1723e6d8f445699d80b93b81a04cf99a
chomu37 <2chanhaeng@gmail.com>
2022-02-18 13:13:14 +0900

    Start when click the timer

--- js/script.js
+++ js/script.js
@@ -345,21 +345,37 @@ function isCycled(obj = connectingTable){
     return length > 2;
 }
 
+function start(){
+    mixAllHex();
+    scoreUp([]);
+}
+
+function reset(){
+    cancelAllCheckedCheckbox();
+    hexes = idList.reduce((acc,curr) => (acc[curr] = 0, acc), {});
+    connectingTable = makeConnectingTable(connectingCable);
+    writeHexes(hexes);
+    displayAllCellPipe(hexes);
+    score = 0;
+}
+
 // make timer
 const timer = document.querySelector('#timer');
 let timerInterval;
+
 function startTimer(limit = 60){
-    const magni = 10;
+    const magni = 100;
     let time = limit * magni;
     let timerId = setInterval(() => {
         time -= 1;
-        timer.querySelector('text').innerHTML = time/10;
+        timer.querySelector('text').innerHTML = time / magni;
         if(isDebug & time % 10 == 0){console.log(time)}
         if(time <= 0){
             toggleTimer();
             timer.querySelector('text').innerHTML = 0;
             // mixAllHex();
-            alertScore()
+            alertScore();
+            reset();
         }
     }, 1000 / magni);
     return timerId;
@@ -372,18 +388,12 @@ function toggleTimer(limit = 60){
     }else{
         score = 0;
         timerInterval = startTimer(limit);
+        start();
     }
 }
-
-timer.addEventListener('click', () => {toggleTimer(20);});
 
 function alertScore(alertedScore = score){
     alert('score : ' + alertedScore);
 }
 
-function start(){
-    mixAllHex();
-    scoreUp([]);
-}
-
-start()
+timer.addEventListener('click', () => {toggleTimer(60);});
